c702939b08637b7e0608a2272211973fe4e0d7f1
santhanalakshmi sundaramurthy <santhanalakshmi.sundaramurthy@underarmour.com>
2021-02-02 18:53:15 +0000

    update the file

---
+++ snapshots/hard_deletes.sql
@@ -0,0 +1,15 @@
+{% snapshot orders_snapshot_hard_delete %}
+
+    {{
+        config(
+          target_schema='snapshots',
+          strategy='timestamp',
+          unique_key='id',
+          updated_at='updated_at',
+          invalidate_hard_deletes=True,
+        )
+    }}
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+{% endsnapshot %}
